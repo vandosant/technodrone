4369f82f14ecf1d5c3f6d01a630f3938247518bb
vandosant <sjskender@gmail.com>
2016-06-18 11:24:46 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to technodrone
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/technodrone/core_test.clj
@@ -0,0 +1,7 @@
+(ns technodrone.core-test
+  (:require [clojure.test :refer :all]
+            [technodrone.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
