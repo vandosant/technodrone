418d61cf829e38bdab4a4579d5df6110553a73ef
vandosant <sjskender@gmail.com>
2016-06-18 18:11:36 -0600

    Add mapify fn to parse rows, normalize to find bad data

--- src/technodrone/core.clj
+++ src/technodrone/core.clj
@@ -1,13 +1,20 @@
 (ns technodrone.core)
 (def filename "Salaries.csv")
-(def job-keys [:employer :salary])
+(def job-keys [:timestamp :employer :location :job-title :years-employed
+               :years-experience :salary])
 
 (defn str->int
   [str]
   (Integer. str))
 
-(def conversions {:employer identity
-                  :salary str->int})
+(def conversions {:timestamp identity
+                  :employer identity
+                  :location identity
+                  :job-title identity
+                  :years-employed identity
+                  :years-experience identity
+                  :salary identity
+                  })
 
 (defn convert
   [job-key value]
@@ -17,3 +24,18 @@
   [string]
   (map #(clojure.string/split % #",")
        (clojure.string/split string #"\n")))
+
+(defn normalize
+  [row]
+  (if (> (count row) 12)
+    (println (count row))
+    row))
+
+(defn mapify
+  [rows]
+  (map (fn [row]
+           (reduce (fn [row-map [job-key value]]
+                     (assoc row-map job-key (convert job-key value)))
+                   {}
+                   (map vector job-keys (normalize row))))
+       (rest rows)))
