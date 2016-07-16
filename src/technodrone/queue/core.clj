d7406682aef8d5f1adadd2449579f770255a9f5b
vandosant <sjskender@gmail.com>
2016-07-15 23:37:23 -0600

    Queue work is executed

--- src/technodrone/queue/core.clj
+++ src/technodrone/queue/core.clj
@@ -3,6 +3,14 @@
 
 (def queue (atom (vector)))
 (def workers (atom (hash-map)))
+(def finished-work (atom (vector)))
+
+(defn results
+  [queue-id]
+  @finished-work)
+
+(defn clear-results []
+  reset! finished-work (vector))
 
 (defn drain
   ([queue-id]
@@ -13,10 +21,13 @@
   ([queue-id next-task]
    (println "Executing next task!")
    (let [current-queue @queue]
-     ;;(eval ((:queue (next-task)) workers)
      (if (empty? current-queue)
        (println "Job's done!")
        (let [next-task (first current-queue)]
+         (swap! finished-work (fn [current-state]
+                               (conj current-state (->>
+                      (get @workers (:queue next-task))
+                      (:task next-task)))))
          (swap! queue (fn [current-state]
                         (rest current-state)))
          (drain queue-id next-task))))))
@@ -33,4 +44,4 @@
 (defn worker [queue-id task-fn]
   (swap! workers
          (fn [current-state]
-           (hash-map current-state {queue-id task-fn}))))
+           (merge current-state {queue-id task-fn}))))
--- test/technodrone/queue/core_test.clj
+++ test/technodrone/queue/core_test.clj
@@ -15,12 +15,23 @@
 
 (deftest draining-queue
   (testing "Draining a queue."
-    (:worker (worker "tester" '(fn [task] (println task))))
+    (:worker (worker "crawler" '(fn [task] (println task))))
     (push "crawler" 1)
     (push "crawler" 2)
     (push "crawler" 3)
     (is (= 3
-           (length "tester")))
+           (length "crawler")))
     (drain "crawler")
     (is (= 0
-           (length "tester")))))
+           (length "crawler")))))
+
+(deftest queue-execution-order
+  (testing "Queue is called in order")
+  (clear-results)
+  (:worker (worker "tester" '(fn [id] id)))
+  (push "tester" 1)
+  (push "tester" 2)
+  (push "tester" 3)
+  (drain "tester")
+  (is (= (results "tester")
+         [1 2 3])))
