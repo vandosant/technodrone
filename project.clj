dede715c357b1529a91140a44e07cc2587cf6069
vandosant <sjskender@gmail.com>
2016-07-30 16:31:40 -0600

    Setup basic compojure handler

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject technodrone "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Data mining and analysis of the tech industry"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -9,7 +9,14 @@
                  [hiccup "1.0.5"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [com.h2database/h2 "1.3.170"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
                  [org.clojure/core.async "0.2.385"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler technodrone.handler/app}
   :main ^:skip-aot technodrone.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles
+  {:uberjar {:aot :all}
+   :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
