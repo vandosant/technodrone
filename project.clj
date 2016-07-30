93dd84212aef5df42841fabe4475b10abbee5ba7
vandosant <sjskender@gmail.com>
2016-07-30 15:16:56 -0600

    Initialize h2 db and add some css

--- project.clj
+++ project.clj
@@ -6,6 +6,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.2.0-beta1"]
                  [org.clojure/data.json "0.2.6"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.170"]
                  [org.clojure/core.async "0.2.385"]]
   :main ^:skip-aot technodrone.core
   :target-path "target/%s"
