e7108c444a3aba798c62c398da2c689051546a96
vandosant <sjskender@gmail.com>
2016-07-30 16:44:15 -0600

    Update handler to manage location routes

--- src/technodrone/handler.clj
+++ src/technodrone/handler.clj
@@ -1,11 +1,20 @@
-(ns my-webapp.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+(ns technodrone.handler
+  (:require [technodrone.views as :views]
+            [compojure.core :as :cc]
+            [compojure.handler :as handler]
+            [compojure.route :as route]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+(cc/defroutes app-routes
+  (cc/GET "/" [] (views/home-page))
+  (cc/GET "/add-location" [] (views/add-location-page))
+  (cc/POST "/add-location" {params :params}
+           (views/add-location-results-page params))
+  (cc/GET "/location/:loc-id" [loc-id]
+          (views/location-page loc-id))
+  (cc/GET "/all-locations" []
+          (views/all-locations-page))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (handler/site app-routes ))
